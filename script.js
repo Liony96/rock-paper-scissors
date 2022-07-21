f6525bcd7fcf91ae8d09cf7497ab124a955dfbec
404 not found <105079159+Liony96@users.noreply.github.com>
2022-07-21 20:48:11 +0200

    added selection functions and counter

--- script.js
+++ script.js
@@ -1,48 +1,98 @@
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"]
-    index = Math.floor(Math.random()*choices.length)
-    chosen = choices[index]
-    return chosen.toUpperCase()
+//  Botones
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+// List of buttons
+const choices = document.querySelectorAll('.choices');
+
+// Screens
+const playerImg = document.querySelector('#playerimage');
+const pcImg = document.querySelector('#pcimage');
+
+//Counters
+let playerCounter = 0;
+let pcCounter = 0;
+
+// Show rock pic when clicked
+rock.addEventListener('click', displayRock);
+rock.addEventListener('click', computerSelection);
+
+
+function displayRock() {
+  playerImg.src = 'images/rock.jpg'
+  playerImg.alt = 'rock'
 }
 
-function getPlayerChoice() {
-    input = prompt("Rock, Scissors or Paper?")
-    return input.toUpperCase()
+// Show paper pic when clicked
+paper.addEventListener('click', displayPaper);
+paper.addEventListener('click', computerSelection);
+
+function displayPaper() {
+  playerImg.src = 'images/paper.jpg';
+  playerImg.alt = 'paper';
 }
 
-let computer = getComputerChoice();
-let player = getPlayerChoice();
-let winMessage = "Congratulations! " + player + " beats " + computer + ". You won!";
-let loseMessage = "Better luck next time! " + computer + " beats " + player + ". You lost!"; 
+// Show scissors pic when clicked
+scissors.addEventListener('click', displayScissors);
+scissors.addEventListener('click', computerSelection);
 
-function playRound(getComputerChoice, getPlayerChoice) {
-    if (computer === player){
-        return "You're tie! Try again."
-    } else if (computer === "ROCK") {
-        if (player === "PAPER") {
-            return winMessage
-        } else if (player === "SCISSORS") {
-            return loseMessage
-        }
-    } else if (computer === "PAPER") {
-        if (player === "ROCK") {
-            return loseMessage
-        } else if (player === "SCISSORS") {
-            return winMessage
-        }
-    } else if (computer === "SCISSORS") {
-        if (player === "ROCK") {
-            return winMessage
-        } else if (player === "PAPER") {
-            return loseMessage
-        }
+function displayScissors() {
+  playerImg.src = 'images/scissors.jpg';
+  playerImg.alt = 'scissors';
+}
+
+//Computer selection
+const options = ['rock', 'paper', 'scissors'];
+
+function computerSelection() {
+  const options = ['rock', 'paper', 'scissors'];
+  rand = Math.floor(Math.random()*options.length);
+  final = options[rand];
+  if (options[rand] === 'rock') {
+    pcImg.src = 'images/rock.jpg';
+    pcImg.alt = 'rock';
+  } else if (options[rand] === 'paper') {
+    pcImg.src = 'images/paper.jpg';
+    pcImg.alt = 'paper';
+  } else if (options[rand] === 'scissors') {
+    pcImg.src = 'images/scissors.jpg';
+    pcImg.alt = 'scissors';
+  }
+  setTimeout(counterFunction, 1000);
+}
+
+
+function counterFunction() {
+  if (pcImg.alt === playerImg.alt) {
+    if (pcImg.alt === 'holder') {
+      return;
+    } else {
+      alert('You are tie. Try again!');
+    } 
+  } else if (playerImg.alt === 'rock') {
+    if (pcImg.alt === 'paper') {
+      pcCounter ++;
+      alert('+1 to Computer. Paper covers rock!');
+    } else if (pcImg.alt === 'scissors') {
+      playerCounter++;
+      alert('+1 to Player. Rocks crushes scissors!');
     }
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getComputerChoice(), player)  
+  } else if (playerImg.alt === 'paper') {
+    if (pcImg.alt === 'rock') {
+      playerCounter ++;
+      alert('+1 to Player. Paper covers rock!');
+    } else if (pcImg.alt === 'scissors') {
+      pcCounter++;
+      alert('+1 to Computer. Scissors cut the paper!');
     }
+  } else if (playerImg.alt === 'scissors') {
+    if (pcImg.alt === 'paper') {
+      playerCounter ++;
+      alert('+1 to Computer. Scissors cut the paper!');
+    } else if (pcImg.alt === 'rock') {
+      pcCounter++;
+      alert('+1 to Player. Rocks crushes scissors!');
+    }
+  }
 }
 
-alert(playRound());
